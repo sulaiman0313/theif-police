61c32277e26a522ab5010ddce4b06f5ff09d6dbe
mohammed sulaiman <sulemanakhtar57@gmail.com>
2023-08-31 19:23:21 +0530

    prommise

--- join.js
+++ join.js
@@ -2,31 +2,46 @@
 
 
 
-function dise(king,theif,minister,police){
-let sqr=Math.floor(Math.random() * 10000)
-console.log(sqr)
+// function dise(king,theif,minister,police){
+// let sqr=Math.floor(Math.random() * 10000)
+// console.log(sqr)
 
-setTimeout(()=>{
-    if(sqr<1000){
-        king()
+// setTimeout(()=>{
+//     if(sqr<1000){
+//         king()
+//     }
+//     else if(sqr==0){
+//         theif()
+//     }
+//     else if(sqr>800){
+//         minister()
+//     }
+//     else{
+//         police()
+//     }
+// } ,1000)
+// }
+// dise(function(){
+//     console.log('king')
+// },function(){
+//     console.log("theif")
+// },function(){
+//     console.log('minister')
+// },function(){
+//     console.log('police')
+// })
+
+
+
+
+//promise
+
+let complete=false;
+let prom=new Promise(function(resolved,reject){
+    if(complete){
+        resolved("successfull");
+    }else{
+        reject("failed");
     }
-    else if(sqr==0){
-        theif()
-    }
-    else if(sqr>800){
-        minister()
-    }
-    else{
-        police()
-    }
-} ,1000)
-}
-dise(function(){
-    console.log('king')
-},function(){
-    console.log("theif")
-},function(){
-    console.log('minister')
-},function(){
-    console.log('police')
-})
+});
+console.log(prom);

c83af72c157e947e9e734da01c9355daf3319943
mohammed sulaiman <sulemanakhtar57@gmail.com>
2023-08-31 13:15:59 +0530

    chor sipahi

---
+++ join.js
@@ -0,0 +1,32 @@
+   
+
+
+
+function dise(king,theif,minister,police){
+let sqr=Math.floor(Math.random() * 10000)
+console.log(sqr)
+
+setTimeout(()=>{
+    if(sqr<1000){
+        king()
+    }
+    else if(sqr==0){
+        theif()
+    }
+    else if(sqr>800){
+        minister()
+    }
+    else{
+        police()
+    }
+} ,1000)
+}
+dise(function(){
+    console.log('king')
+},function(){
+    console.log("theif")
+},function(){
+    console.log('minister')
+},function(){
+    console.log('police')
+})
